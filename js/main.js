e3f1d7e499b6fe807142c0d86d91feba51abd9e2
Roxolana363 <ilkiv.roxy@gmail.com>
2020-05-31 23:06:31 +0300

    grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
- 
-    int clicks = 0;
-    function click() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    };
+
+    function count(){
+        if (!this.counter) {
+            this.counter = 0
+        };
+         this.innerHTML = ++this.counter;
+         console.log(this.counter);
+         document.getElementById("qq").innerHTML = this.counter;
+    }
+
+
   
